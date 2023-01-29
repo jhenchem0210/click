febde968016c6adbe84c7ae4521cfb5c4f083375
jhenchem0210 <jhenchem0210@gmail.com>
2023-01-29 17:18:45 +0800

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
