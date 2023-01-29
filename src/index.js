bf3a1ffc48a40453e87b649c6c56c5e3f8dc605e
jhenchem0210 <jhenchem0210@gmail.com>
2023-01-29 15:58:30 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Login.jsx 	new file:   src/Register.jsx 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
-import { useState } from "react";
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Importing the Bootstrap 5 CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyForm() {
-  const [inputs, setInputs] = useState({});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs (values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault ();
-    console.log(inputs);
-
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-        <input 
-          type="text" 
-          name= "username"
-          value = {inputs.username || ""}
-          onChange= {handleChange}
-        />
-      </label>
-
-      <label>Enter your AGE:
-        <input 
-          type="number" 
-          name= "age"
-          value = {inputs.age || ""}
-          onChange= {handleChange}
-        />
-      </label>
-
-      <input type="submit" />
-    </form>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+reportWebVitals();
